c5c306a18b3fb911e6a983f44d063e7b756f6634
Harkame <louisr.daviaud@gmail.com>
2015-11-02 21:46:27 +0100

    Update Identification.java

--- Identification.java
+++ Identification.java
@@ -1,173 +1,86 @@
 package projet_bulles;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Frame;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 
-import utilitaire.structure.Repertoire;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
-public class Utilisateur {
-	private String identifiant;
-	private static File fichier = new File("D:/utilisateurs.txt");
-	private Score[] scores;
+public class Identification extends JFrame {
 
-	public Utilisateur(String p_identifiant) {
-		this.identifiant = p_identifiant.toLowerCase();
-		this.scores = new Score[10];
-		for (int i = 0; i < this.scores.length; i++) {
-			this.scores[i] = new Score();
-		}
-	}
+	private JPanel fenetre;
 
-	/**
-	 * public Utilisateur trouverUtilisateur() throws IOException { try { int i
-	 * = 0; StringBuilder ligne = new StringBuilder(); BufferedReader lecteur =
-	 * new BufferedReader(new FileReader(fichier)); lecteur = new
-	 * BufferedReader(new FileReader(fichier)); while (true) {
-	 * ligne.setLength(0); try { ligne.append(lecteur.readLine()); i++; } catch
-	 * (IOException e) { } } return ligne.toString(); } catch
-	 * (FileNotFoundException e) { } return null; } }
-	 * 
-	 * @throws IOException
-	 **/
+	JLabel myLabel;
+	private JButton bouton_seconnecter;
+	private static String a;
+	JPanel top;
+	private JPanel container = new JPanel();
+	private JTextField jtf = new JTextField();
+	private JLabel label = new JLabel("Identifiant");
+	private static Utilisateur utilisateur;
 
-	private StringBuilder recupLigne() throws IOException {
-		String ligne = new String();
-		BufferedReader lecteur = new BufferedReader(new FileReader(fichier));
-		StringBuilder identifiant = new StringBuilder();
-		while ((ligne = lecteur.readLine()) != null) {
-			identifiant.append(recupIdentifiant(ligne));
-			if (identifiant.toString().equals(this.identifiant)) {
-				lecteur.close();
-				identifiant.setLength(0);
-				identifiant.append(ligne);
-				return identifiant;
-			} else {
-				identifiant.setLength(0);
-			}
-		}
-		lecteur.close();
-		return null;
-	}
-
-	private StringBuilder recupIdentifiant(String ligne) {
-		StringBuilder identifiant = new StringBuilder();
-		int i = 0;
-		while (ligne.charAt(i) != '|') {
-			identifiant.append(ligne.charAt(i));
-			i++;
-		}
-		return identifiant;
-	}
-
-	private void recupScores(StringBuilder ligne) {
-		StringBuilder scores = new StringBuilder();
-		StringBuilder strb_ligne = new StringBuilder();
-		int i = 0;
-		while (ligne.charAt(i) != '|') {
-			i++;
-		}
-		strb_ligne.append(ligne);
-		strb_ligne.delete(0, i);
-		for (i = 0; i < this.scores.length; i++) {
-			this.scores[i].recupScore(strb_ligne);
-		}
-	}
-
-	private void sauvegarderUtilisateur() throws IOException {
-		FileWriter fw = new FileWriter(fichier, true);
-		fw.write(this.identifiant.toString() + '|');
-		for (int i = 0; i < 10; i++) {
-			fw.write("0-0|");
-		}
-		fw.write(System.getProperty("line.separator"));
-		fw.close();
-	}
-
-	public void Identification() throws IOException {
-		if (this.UtilisateurExistant()) {
-			StringBuilder informations = this.recupLigne();
-			this.recupIdentifiant(informations.toString());
-			this.recupScores(informations);
-		} else {
-			this.sauvegarderUtilisateur();
-		}
-	}
-
-	/**
-	 * 
-	 * @return
-	 * @throws IOException
-	 */
-	public boolean UtilisateurExistant() throws IOException {
-		String ligne = new String();
-		BufferedReader lecteur = new BufferedReader(new FileReader(fichier));
-		StringBuilder identifiant = new StringBuilder();
-		while ((ligne = lecteur.readLine()) != null) {
-			identifiant.append(recupIdentifiant(ligne));
-			if (identifiant.toString().equals(this.identifiant)) {
-				lecteur.close();
-				return true;
-			} else {
-				identifiant.setLength(0);
-			}
-		}
-		lecteur.close();
-		return false;
-	}
-
-	private static String lireFichier(File fichier, int numero_ligne) {
-		try {
-			int i = 0;
-			StringBuilder ligne = new StringBuilder();
-			BufferedReader lecteur = new BufferedReader(new FileReader(fichier));
-			lecteur = new BufferedReader(new FileReader(fichier));
-			while (i <= numero_ligne) {
-				ligne.setLength(0);
+	public Identification() {
+		this.setTitle("La case à bulles");
+		this.setExtendedState(Frame.MAXIMIZED_BOTH);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLocationRelativeTo(null);
+		container.setBackground(Color.white);
+		// container.setLayout(new BorderLayout());
+		Font police = new Font("Arial", Font.BOLD, 20);
+		jtf.setFont(police);
+		jtf.setPreferredSize(new Dimension(150, 30));
+		this.bouton_seconnecter = new JButton("Se connecter");
+		this.bouton_seconnecter.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				utilisateur = new Utilisateur(jtf.getText() == null ? "" : jtf
+						.getText());
 				try {
-					ligne.append(lecteur.readLine());
-					i++;
-				} catch (IOException e) {
+					utilisateur.Identification();
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
 				}
+				System.out.println(utilisateur.toString());
 			}
-			return ligne.toString();
-		} catch (FileNotFoundException e) {
-		}
-		return null;
+		});
+		// jtf.setForeground(Color.BLACK);
+		container.add(jtf, BorderLayout.CENTER);
+		container.add(bouton_seconnecter, BorderLayout.SOUTH);
+		this.setContentPane(container);
+		this.setVisible(true);
 	}
 
-	public String toString() {
-		StringBuilder utilisateur = new StringBuilder();
-		utilisateur.append(this.identifiant
-				+ System.getProperty("line.separator"));
-		for (int i = 0; i < this.scores.length; i++) {
-			switch (i) {
-			default:
-				utilisateur.append("Pallier " + (i + 1) + "  ~ "
-						+ this.scores[i].toString()
-						+ System.getProperty("line.separator"));
-				break;
-			case 9:
-				utilisateur.append("Pallier " + (i + 1) + " ~ "
-						+ this.scores[i].toString()
-						+ System.getProperty("line.separator"));
-				break;
+	public String execute() {
+		bouton_seconnecter.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				utilisateur = new Utilisateur(jtf.getText());
+				try {
+					utilisateur.Identification();
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				System.out.println(utilisateur.toString());
 			}
-		}
-		return utilisateur.toString();
+		});
+		return a;
 	}
 
-	public static void main(String[] Args) throws IOException {
-		Utilisateur u1 = new Utilisateur("Alain");
-		Utilisateur u2 = new Utilisateur("Robin");
-		Utilisateur u3 = new Utilisateur("Remi");
-		Utilisateur u4 = new Utilisateur("Meddy");
-		Utilisateur u5 = new Utilisateur("Meddy");
-		u1.Identification();
-		System.out.println(u1.toString());
-		// System.out.println(u1.toString());
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Identification id1 = new Identification();
+		// id1.execute();
 	}
 }
