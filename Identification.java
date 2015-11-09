b383cb964e99edb9afee3d018fb53b49c582f233
Harkame <louisr.daviaud@gmail.com>
2015-11-09 10:56:01 +0100

    Update Identification.java

--- Identification.java
+++ Identification.java
@@ -18,31 +18,31 @@ import javax.swing.JTextField;
 
 public class Identification extends JFrame {
 
-	private JPanel fenetre;
+	// private JPanel fenetre;
 
 	JLabel myLabel;
 	private JButton bouton_seconnecter;
 	private static String a;
 	JPanel top;
-	private JPanel container = new JPanel();
-	private JTextField jtf = new JTextField();
+	private JPanel fenetre = new JPanel();
+	private JTextField champ_saisie = new JTextField();
 	private JLabel label = new JLabel("Identifiant");
 	private static Utilisateur utilisateur;
 
 	public Identification() {
-		this.setTitle("La case à bulles");
-		this.setExtendedState(Frame.MAXIMIZED_BOTH);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setLocationRelativeTo(null);
-		container.setBackground(Color.white);
-		// container.setLayout(new BorderLayout());
-		Font police = new Font("Arial", Font.BOLD, 20);
-		jtf.setFont(police);
-		jtf.setPreferredSize(new Dimension(150, 30));
+		this.setTitle("La case à bulles"); //Donne un titre à la fenêtre
+		this.setExtendedState(Frame.MAXIMIZED_BOTH); //Met la fenêtre en plein écran
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Si on clic sur la croix rouge, le process se termine
+		this.setLocationRelativeTo(null); //La fenêtre est "independante"
+		fenetre.setBackground(Color.white); //Le fond est bleu
+		// fenetre.setLayout(new BorderLayout());
+		Font police = new Font("Arial", Font.BOLD, 20); //Définition de la police d'écriture et de sa taille
+		champ_saisie.setFont(police); //On active la police dans le champ
+		champ_saisie.setPreferredSize(new Dimension(150, 30));
 		this.bouton_seconnecter = new JButton("Se connecter");
 		this.bouton_seconnecter.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				utilisateur = new Utilisateur(jtf.getText() == null ? "" : jtf
+				utilisateur = new Utilisateur(champ_saisie.getText() == null ? "" : champ_saisie
 						.getText());
 				try {
 					utilisateur.Identification();
@@ -53,17 +53,17 @@ public class Identification extends JFrame {
 				System.out.println(utilisateur.toString());
 			}
 		});
-		// jtf.setForeground(Color.BLACK);
-		container.add(jtf, BorderLayout.CENTER);
-		container.add(bouton_seconnecter, BorderLayout.SOUTH);
-		this.setContentPane(container);
+		// champ_saisie.setForeground(Color.BLACK);
+		fenetre.add(champ_saisie, BorderLayout.CENTER);
+		fenetre.add(bouton_seconnecter, BorderLayout.SOUTH);
+		this.setContentPane(fenetre);
 		this.setVisible(true);
 	}
 
 	public String execute() {
 		bouton_seconnecter.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				utilisateur = new Utilisateur(jtf.getText());
+				utilisateur = new Utilisateur(champ_saisie.getText());
 				try {
 					utilisateur.Identification();
 				} catch (IOException e1) {
